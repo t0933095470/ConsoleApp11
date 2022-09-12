753506ed37b1a57f6dccafe45de347df39b10c10
t0933095470 <thethao165@gmail.com>
2022-09-12 09:39:51 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//While loop
+
+bool isFound = false;
+int value = 0;
+while (isFound != true) //check whether or not run code inside its block
+{
+    if(value == 99)
+    {
+        isFound = true;
+    }
+    value = value + 3;
+    Console.WriteLine(value);
+}
